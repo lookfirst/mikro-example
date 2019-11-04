3f9f37be213517845c0c3be318acc818ba7473c0
Jon Stevens <latchkey@gmail.com>
2019-11-04 17:36:39 +0700

    add cool test of unit of work

--- test/db/rig.spec.ts
+++ test/db/rig.spec.ts
@@ -44,4 +44,34 @@ describe('rigs', () => {
 
 		console.log(result);
 	});
+
+	test('unit of works... so nice!', async () => {
+		const rig1 = new Rig();
+		const rigType = new RigType();
+		rigType.id = 1;
+		rigType.name = 'rigType 1';
+
+		rig1.id = 1;
+		rig1.type = rigType;
+
+		await orm.em.persistAndFlush(rig1);
+		orm.em.clear();
+
+		const rigChange = await orm.em.findOneOrFail(Rig, rig1.id);
+		rigChange.type.name = 'removed';
+
+		const rigType2 = new RigType();
+		rigType2.id = 2;
+		rigType2.name = 'rigType 2';
+		rigChange.type = rigType2;
+
+		await orm.em.persistAndFlush(rigChange);
+		orm.em.clear();
+
+		const rigType1Db = await orm.em.findOneOrFail(RigType, rigType.id);
+		expect(rigType1Db.name).toBe('removed');
+
+		const rigAgain = await orm.em.findOneOrFail(Rig, rig1.id, {populate: true});
+		expect(rigAgain.type.name).toBe('rigType 2');
+	});
 });
